b8267d397544b22fe28fa69694aee5c8f2026f33
Sam Green <samsfgreen@gmail.com>
2023-01-11 12:49:36 +0000

    fixing type errors on about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -11,6 +11,13 @@ import { FOCUS_VISIBLE_OUTLINE } from "@/styles/constants";
 import { RoughNotation } from "react-rough-notation";
 import colors from "tailwindcss/colors";
 
+const ExtLink = ({ href, children }: { href: string; children: any }) => (
+  <a target="_blank" rel="noopener noreferrer" href={href}>
+    {" "}
+    {children}
+  </a>
+);
+
 export default function About() {
   return (
     <Layout>
@@ -23,64 +30,41 @@ export default function About() {
               Co-founder
             </RoughNotation>{" "}
             and Head of Product at{" "}
-            <a target="_blank" href="https://turinglab.co.uk">
-              Turinglab
-            </a>{" "}
-            where I build products to make coding education more fun and
-            effective.
+            <ExtLink href="https://turinglab.co.uk">Turinglab</ExtLink> where I
+            build products to make coding education more fun and effective.
           </p>
         </EmojiSectionWrapper>
         <EmojiSectionWrapper>
           <p>⚡</p>
           <p>
-            I've help bootstrap Turinglab to over £1m in revenue and teach over
-            125,000 kids to code across over 700,000 hours of learning, reaching
-            up to 80,000 learners per month.
+            I{"'"}ve help bootstrap Turinglab to over £1m in revenue and teach
+            over 125,000 kids to code across over 700,000 hours of learning,
+            reaching up to 80,000 learners per month.
           </p>
         </EmojiSectionWrapper>
         <EmojiSectionWrapper>
           <p>🤝</p>
           <p>
             I’ve managed projects at scale with clients such as
-            <a
-              target="_blank"
-              href="https://www.aboutamazon.co.uk/news/community/sign-up-to-our-free-amazon-future-engineer-virtual-coding-programme"
-            >
-              {" "}
+            <ExtLink href="https://www.aboutamazon.co.uk/news/community/sign-up-to-our-free-amazon-future-engineer-virtual-coding-programme">
               Amazon
-            </a>
+            </ExtLink>
             ,
-            <a
-              target="_blank"
-              href="https://www.youtube.com/watch?v=ZSY2Rwc2MUw&ab_channel=YOOXNET-A-PORTER"
-            >
-              {" "}
+            <ExtLink href="https://www.youtube.com/watch?v=ZSY2Rwc2MUw&ab_channel=YOOXNET-A-PORTER">
               Net-a-Porter
-            </a>
+            </ExtLink>
             ,
-            <a
-              target="_blank"
-              href="https://edtechnology.co.uk/latest-news/uks-first-curriculum-linked-coding-platform-launched/"
-            >
-              {" "}
+            <ExtLink href="https://edtechnology.co.uk/latest-news/uks-first-curriculum-linked-coding-platform-launched/">
               DCMS
-            </a>
+            </ExtLink>
             ,
-            <a
-              target="_blank"
-              href="https://www.youth.om/en/the-program/al-nashia-scheme"
-            >
-              {" "}
+            <ExtLink href="https://www.youth.om/en/the-program/al-nashia-scheme">
               The Sultanate of Oman
-            </a>
+            </ExtLink>
             ,
-            <a
-              target="_blank"
-              href="https://www.imperial.ac.uk/computing/outreach/codelab/"
-            >
-              {" "}
+            <ExtLink href="https://www.imperial.ac.uk/computing/outreach/codelab/">
               Imperial College London
-            </a>{" "}
+            </ExtLink>{" "}
             and more.
           </p>
         </EmojiSectionWrapper>
@@ -88,128 +72,96 @@ export default function About() {
           <p>👑</p>
           <p>
             I’ve even taught{" "}
-            <a
-              target="_blank"
-              href="https://www.imperial.ac.uk/news/186339/prince-charles-given-insight-into-outreach/"
-            >
+            <ExtLink href="https://www.imperial.ac.uk/news/186339/prince-charles-given-insight-into-outreach/">
               Prince Charles (now King Charles III) and Camilla coding!
-            </a>
+            </ExtLink>
           </p>
         </EmojiSectionWrapper>
         <EmojiSectionWrapper>
           <p>🎓</p>
           <p>
-            I met my cofounder while studying Computer Science at{" "}
-            <a
-              className="text-grey-dark"
-              target="_blank"
-              href="https://www.imperial.ac.uk/computing/"
-            >
-              <i>Imperial College London</i>
-            </a>{" "}
-            where I brought together my love of design, data and machine
-            learning to my thesis{" "}
-            <a
-              target="_blank"
-              href="/pdfs/Visualising Deep Neural Networks.pdf"
-            >
-              'Visualising Deep Neural Networks'
-            </a>
+            I met my cofounder while studying Computer Science at Imperial
+            College London where I brought together my love of design, data and
+            machine learning to my thesis{" "}
+            <ExtLink href="/pdfs/Visualising Deep Neural Networks.pdf">
+              <RoughNotation
+                type="highlight"
+                show={true}
+                color={colors.sky[200]}
+              >
+                Visualising Deep Neural Networks
+              </RoughNotation>
+              .
+            </ExtLink>
+          </p>
+        </EmojiSectionWrapper>
+        <EmojiSectionWrapper>
+          <p>🤖</p>
+          <p>
+            After graduating I contracted briefly as a software engineer
+            building products with Machine Learning for{" "}
+            <ExtLink href="https://www.proquoai.com/">
+              <i>TX.ai</i>
+            </ExtLink>
             .
           </p>
         </EmojiSectionWrapper>
-        {/* <EmojiSectionWrapper>
-      <p>🤖</p>
-      <p>After graduating I contracted briefly as a software engineer building products with Machine Learning for <a className="text-grey-dark" href="https://www.proquoai.com/"><i>ProQuo AI.</i></a></p>
-    </EmojiSectionWrapper> */}
         <EmojiSectionWrapper>
           <p>🐺</p>
           <p>
             I was an early employee at{" "}
-            <a
-              className="text-grey-dark"
-              target="_blank"
-              href="https://www.joinef.com/"
-            >
+            <ExtLink href="https://www.joinef.com/">
               <i>Entrepreneur First</i>
-            </a>{" "}
-            on their Program Team. It was working at EF with technical founders
-            that inspired me to study Computer Science.
+            </ExtLink>{" "}
+            where I worked on their Program Team. It was working at EF with
+            technical founders that inspired me to study Computer Science.
           </p>
         </EmojiSectionWrapper>
         <EmojiSectionWrapper>
           <p>🏡</p>
           <p>
-            I started out as designer for{" "}
-            <a
-              className="text-grey-dark"
-              target="_blank"
-              href="https://www.urbansplash.co.uk/"
-            >
+            I started out as designer for
+            <ExtLink href="https://www.urbansplash.co.uk/">
               <i>Urban Splash</i>
-            </a>{" "}
+            </ExtLink>
             and later at{" "}
-            <a
-              className="text-grey-dark"
-              target="_blank"
-              href="https://www.alisonbrooksarchitects.com/"
-            >
+            <ExtLink href="https://www.alisonbrooksarchitects.com/">
               <i>Alison Brooks Architects</i>
-            </a>{" "}
+            </ExtLink>{" "}
             working on several urban and architectural designs.
           </p>
         </EmojiSectionWrapper>
         <EmojiSectionWrapper>
           <p>🎨</p>
           <p>
-            I learned design at the{" "}
-            <a
-              target="_blank"
-              className="text-grey-dark"
-              href="https://www.arct.cam.ac.uk/"
-            >
-              <i>University of Cambridge</i>
-            </a>{" "}
-            where I studied Architecture and wrote my thesis exploring the{" "}
-            <a target="_blank" href="/pdfs/Added Value of Quality Design.pdf">
-              'Adding Value through Quality Design'
-            </a>
-            . Here I also ran design lectures featuring industry giants such as{" "}
-            <a
-              className="text-grey-dark"
-              target="_blank"
-              href="https://www.youtube.com/watch?v=lTcIQ7Rd46w&ab_channel=CambridgeUnion"
-            >
+            I learned design studying architecture at{" "}
+            <ExtLink href="https://www.arct.cam.ac.uk/">
+              University of Cambridge
+            </ExtLink>
+            where I studied Architecture and wrote my thesis exploring the
+            <ExtLink href="/pdfs/Added Value of Quality Design.pdf">
+              Adding Value through Quality Design
+            </ExtLink>
+            . Here I also ran design lectures featuring industry giants such as
+            <ExtLink href="https://www.youtube.com/watch?v=lTcIQ7Rd46w&ab_channel=CambridgeUnion">
               Chris Dercon
-            </a>{" "}
-            and{" "}
-            <a
-              target="_blank"
-              className="text-grey-dark"
-              href="https://www.fosterandpartners.com/studio/spencer-de-grey/"
-            >
-              <i>Spencer De Grey</i>
-            </a>{" "}
-            as well as new, up and coming designers.
+            </ExtLink>
+            , Spencer De Grey and as well as new, up and coming designers.
           </p>
         </EmojiSectionWrapper>
         <EmojiSectionWrapper>
           <p>🏕</p>
           <p>
-            If I'm not thinking about building impactful digital products then
-            I'm trying to get outside. I'm a passionate climber and have climbed
-            all over Europe. I've even helped a former{" "}
-            <a
-              target="_blank"
-              className="text-grey-dark"
-              href="https://www.instagram.com/captaincutloose/?hl=en"
-            >
+            If I{"'"}m not thinking about building impactful digital products
+            then I{"'"}m trying to get outside. I{"'"}m a passionate climber and
+            have climbed all over Europe. I{"'"}ve even helped a former
+            <ExtLink href="https://www.instagram.com/captaincutloose/?hl=en">
               <i>GB Climber</i>
-            </a>{" "}
-            develop a coaching manual and{" "}
-            <a target="_blank" href="https://greenjosh.com/catalyst.html">
+            </ExtLink>{" "}
+            develop a coaching manual and
+            <ExtLink href="https://greenjosh.com/catalyst.html">
               mobile app
-            </a>
+            </ExtLink>
             .
           </p>
         </EmojiSectionWrapper>
